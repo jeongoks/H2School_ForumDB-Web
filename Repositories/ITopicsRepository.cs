ba40e66ba58ab14a50b2260941afb4a8300618dd
Jasmin Bidstrup Nielsen <jasm3956@elevcampus.dk>
2021-03-11 15:10:48 +0100

    Version 0.2.0 - Creating the Interface for the Topic Repository.

--- Repositories/ITopicsRepository.cs
+++ Repositories/ITopicsRepository.cs
@@ -12,6 +12,10 @@ namespace H2School_ForumDB_Web.Repositories
         Topic CreateTopic(string headLine, string bodyText, User user, Category category);
         void DeleteTopic(Topic topic);
         List<Topic> GetAllTopics();
-
+        Topic GetTopicByID(int id);
+        List<Topic> GetTopicByCategory(Category category);
+        List<Topic> GetTopicByAuthor(User user);
+        List<Topic> GetTopicByDate(DateTime date);
+        Topic UpdateTopic(Topic topic, string headLine, string bodyText);
     }
 }
